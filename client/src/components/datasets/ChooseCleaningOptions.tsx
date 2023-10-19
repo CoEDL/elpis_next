30730b0902455e60c1fd1df1ae3b261bafa7e31c
Harry Keightley <harrykeightley@outlook.com>
2023-10-19 17:27:09 +1000

    Add shadcn Separator

--- client/src/components/datasets/ChooseCleaningOptions.tsx
+++ client/src/components/datasets/ChooseCleaningOptions.tsx
@@ -5,7 +5,6 @@ import {Input} from 'components/ui/input';
 import {Label} from 'components/ui/label';
 import {useAtom} from 'jotai';
 import React, {useState} from 'react';
-import {X} from 'react-feather';
 import {cleaningOptionsAtom} from 'store';
 import {CleaningOptions} from 'types/Dataset';
 
